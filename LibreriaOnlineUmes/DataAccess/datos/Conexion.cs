63fcb5116b92b8f425c87e541ba958be087fd8d4
Jose-Obregon <jobregon2121@gmail.com>
2017-04-03 10:20:49 -0600

    Add files via upload

---
+++ LibreriaOnlineUmes/DataAccess/datos/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace DataAccess.datos
+{
+    public class Conexion
+    {
+        public SqlConnection conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=173.209.236.173;Initial Catalog=kardex_1_1;User ID=sa;Password=umes123!";
+            return cn;
+        }
+
+    }
+}
